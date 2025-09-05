2358a7cb4360c9b0aa8ac17a0ac44aee2ff52f7e
tiffany <tiffany871205@gmail.com>
2025-09-05 17:37:22 +0800

    fix:修改test第二題答案為第一選項

--- js/test.js
+++ js/test.js
@@ -19,9 +19,9 @@ const questions = [
     kk: "[ləʊn]",
     dj: "[ləʊn]",
     options: [
-      { option: "v. 貸款、借款", correct: false },
+      { option: "v. 貸款、借款", correct: true },
       { option: "n. 一條麵包", correct: false },
-      { option: "n. 土壤", correct: true },
+      { option: "n. 土壤", correct: false },
       { option: "v. 爭論、爭端", correct: false }
     ]
   }
