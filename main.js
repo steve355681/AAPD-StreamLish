55e714fa750189b97c295bb8f488a5aa0605b25a
wc-su <theblissout@gmail.com>
2025-09-09 13:46:42 +0800

    feat: add review page

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
 import './assets/scss/all.scss';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import * as bootstrap from "bootstrap"
+
 
 console.log('Hello world');
+
+document.addEventListener("DOMContentLoaded", () => {
+  const hash = window.location.hash;
+  if (hash) {
+    const triggerEl = document.querySelector(`a[href="${hash}"]`);
+    if (triggerEl) {
+      new bootstrap.Tab(triggerEl).show();
+    }
+
+    // 移除網址 hash，但不重新載入頁面
+    history.replaceState(null, null, window.location.pathname);
+  }
+});
